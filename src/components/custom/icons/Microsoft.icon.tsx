9ac5426c705cee74f32b00354d512985736dabdc
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-08 00:20:27 +0400

    Custom Icons

---
+++ src/components/custom/icons/Microsoft.icon.tsx
@@ -0,0 +1,22 @@
+import SvgIcon from "@mui/material/SvgIcon";
+
+const MicrosoftIcon = () => {
+  return (
+    <SvgIcon>
+      <svg
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path fill="#F35325" d="M1 1h6.5v6.5H1V1z"></path>
+        <path fill="#81BC06" d="M8.5 1H15v6.5H8.5V1z"></path>
+        <path fill="#05A6F0" d="M1 8.5h6.5V15H1V8.5z"></path>
+        <path fill="#FFBA08" d="M8.5 8.5H15V15H8.5V8.5z"></path>
+      </svg>
+    </SvgIcon>
+  );
+};
+
+export default MicrosoftIcon;
---
+++ src/components/custom/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as BotuIcon } from "./Botu.icon";
+export { default as GoogleIcon } from "./Google.icon";
+export { default as MicrosoftIcon } from "./Microsoft.icon";
