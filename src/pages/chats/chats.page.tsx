1260d1770f16b429cd6662d971c348974ca8ab4b
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-08 00:11:01 +0400

    Chats Page

---
+++ src/pages/chats/chats.page.tsx
@@ -0,0 +1,261 @@
+import { useSearchParams } from "react-router-dom";
+
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+
+import Divider from "@mui/material/Divider";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Avatar from "@mui/material/Avatar";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import IconButton from "@mui/material/IconButton";
+
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
+import FormatColorFillIcon from "@mui/icons-material/FormatColorFill";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import React from "react";
+import { Search } from "@/components";
+
+const ChatsPage = () => {
+  // @ts-ignore
+  const [searchParams, setSearchParams] = useSearchParams();
+  // @ts-ignore
+  const [formats, setFormats] = React.useState(() => ["bold", "italic"]);
+  // @ts-ignore
+  const handleFormat = (event: React.MouseEvent<HTMLElement>, newFormats: string[]) => {
+    setFormats(newFormats);
+  };
+  return (
+    <>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", height: "100%", width: "100%", pb: "70px" }}
+      >
+        <Stack direction={"row"} sx={{ width: "100%", pt: 1.5, pb: 3.5, px: 3 }}>
+          <Stack direction="row" sx={{ gap: 1 }}>
+            <Search />
+            <IconButton>
+              <EditNoteIcon />
+            </IconButton>
+          </Stack>
+        </Stack>
+
+        <Stack
+          spacing={{ xs: 1, sm: 2 }}
+          sx={{ height: "100%", flexWrap: "wrap", mx: 3, pb: "70px" }}
+          direction="row"
+          useFlexGap
+        >
+          <Paper
+            variant={"outlined"}
+            sx={{ width: "100%", maxWidth: 360, height: "100%", overflow: "auto" }}
+          >
+            <List>
+              <ListSubheader>Active</ListSubheader>
+
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Brunch this weekend?"
+                  secondary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        sx={{ color: "text.primary", display: "inline" }}
+                      >
+                        Ali Connors
+                      </Typography>
+                      {" — I'll be in your neighborhood doing errands this…"}
+                    </>
+                  }
+                />
+              </ListItem>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Brunch this weekend?"
+                  secondary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        sx={{ color: "text.primary", display: "inline" }}
+                      >
+                        Ali Connors
+                      </Typography>
+                      {" — I'll be in your neighborhood doing errands this…"}
+                    </>
+                  }
+                />
+              </ListItem>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Brunch this weekend?"
+                  secondary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        sx={{ color: "text.primary", display: "inline" }}
+                      >
+                        Ali Connors
+                      </Typography>
+                      {" — I'll be in your neighborhood doing errands this…"}
+                    </>
+                  }
+                />
+              </ListItem>
+              <ListSubheader>Today</ListSubheader>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Oui Oui"
+                  secondary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        sx={{ color: "text.primary", display: "inline" }}
+                      >
+                        Sandra Adams
+                      </Typography>
+                      {" — Do you have Paris recommendations? Have you ever…"}
+                    </>
+                  }
+                />
+              </ListItem>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Brunch this weekend?"
+                  secondary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        sx={{ color: "text.primary", display: "inline" }}
+                      >
+                        Ali Connors
+                      </Typography>
+                      {" — I'll be in your neighborhood doing errands this…"}
+                    </>
+                  }
+                />
+              </ListItem>
+              <Divider />
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Brunch this weekend?"
+                  secondary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        sx={{ color: "text.primary", display: "inline" }}
+                      >
+                        Ali Connors
+                      </Typography>
+                      {" — I'll be in your neighborhood doing errands this…"}
+                    </>
+                  }
+                />
+              </ListItem>
+              <ListSubheader>Aug 3</ListSubheader>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Brunch this weekend?"
+                  secondary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        sx={{ color: "text.primary", display: "inline" }}
+                      >
+                        Ali Connors
+                      </Typography>
+                      {" — I'll be in your neighborhood doing errands this…"}
+                    </>
+                  }
+                />
+              </ListItem>
+              <Divider />
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Brunch this weekend?"
+                  secondary={
+                    <>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        sx={{ color: "text.primary", display: "inline" }}
+                      >
+                        Ali Connors
+                      </Typography>
+                      {" — I'll be in your neighborhood doing errands this…"}
+                    </>
+                  }
+                />
+              </ListItem>
+              <Divider />
+            </List>
+          </Paper>
+          <Paper sx={{ flexGrow: 1 }} variant={"outlined"}>
+            <OutlinedInput></OutlinedInput>
+            <ToggleButtonGroup
+              value={formats}
+              onChange={handleFormat}
+              aria-label="text formatting"
+              size={"small"}
+            >
+              <ToggleButton value="bold" aria-label="bold">
+                <FormatBoldIcon />
+              </ToggleButton>
+              <ToggleButton value="italic" aria-label="italic">
+                <FormatItalicIcon />
+              </ToggleButton>
+              <ToggleButton value="underlined" aria-label="underlined">
+                <FormatUnderlinedIcon />
+              </ToggleButton>
+              <ToggleButton value="color" aria-label="color" disabled>
+                <FormatColorFillIcon />
+                <ArrowDropDownIcon />
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Paper>
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+export default ChatsPage;
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+import { lazy } from "react";
+
+const ChatsPage = lazy(() => import("./chats/chats.page"));
+
+export { ChatsPage };
