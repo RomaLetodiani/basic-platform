d9a5be830eea1ae8b1be1df30e83ef81ed9ff8d5
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-08 00:18:51 +0400

    lib index export

---
+++ src/lib/index.ts
@@ -0,0 +1,3 @@
+export { default as getPlatformTheme } from "./theme/getPlatformTheme";
+export { default as platformAPI } from "./axios/platformAPI";
+export { default as convoAPI } from "./axios/convoAPI";
