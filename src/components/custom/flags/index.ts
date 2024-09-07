308335502b5c691fe4a1b7dbc1105cac1e076ec7
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-08 00:20:37 +0400

    Custom Flags

---
+++ src/components/custom/flags/index.ts
@@ -0,0 +1,6 @@
+import BrazilFlag from "./Brazil.flag";
+import GlobeFlag from "./Globe.flag";
+import IndianFlag from "./Indian.flag";
+import UsaFlag from "./Usa.flag";
+
+export { BrazilFlag, GlobeFlag, IndianFlag, UsaFlag };
