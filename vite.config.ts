47eaa1483b8ea2c478620b52a81f46f38edd6fe8
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-08 01:13:22 +0400

    vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: "eslint . --ext ts,tsx --report-unused-disable-directives --max-warnings 0",
+      },
+    }),
+  ],
   build: {
     chunkSizeWarningLimit: 1000,
   },
