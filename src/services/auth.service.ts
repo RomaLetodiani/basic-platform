d154864f60fe5276e0e2ff87f615012aab86c2ef
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-08 12:34:21 +0400

    Auth Services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { platformAPI } from "@/lib";
 
 const AuthServices = {
-  localLogin: (body: { email: string; password: string }) =>
+  localLogin: (body: { email: string; password: string }): Promise<string> =>
     platformAPI.post("auth/local/login", body),
+  googleLogin: (access_token: string): Promise<string> =>
+    platformAPI.post("auth/google/login", { access_token }),
   getCurrentUser: () => platformAPI.get("auth/me"),
 };
 
