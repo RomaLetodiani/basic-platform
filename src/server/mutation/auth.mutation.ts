312d87a70a60082a6e9065456d1c78399b31cffd
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-08 12:34:12 +0400

    useAuthMutations

---
+++ src/server/mutation/auth.mutation.ts
@@ -0,0 +1,36 @@
+import { useMutation } from "@tanstack/react-query";
+import { AuthServices } from "@/services";
+import { useCallback } from "react";
+import { AuthStore } from "@/stores";
+
+const useAuthMutations = () => {
+  const setTokens = AuthStore((state) => state.setTokens);
+
+  const onSuccess = useCallback(
+    (accessToken: string) => {
+      setTokens(accessToken);
+      console.log("Login successful");
+    },
+    [setTokens],
+  );
+
+  const onError = useCallback((error: Error) => {
+    console.error("❌ ~ AuthServices.login ~ error", error);
+  }, []);
+
+  const loginMutation = useMutation<string, Error, any>({
+    mutationFn: AuthServices.localLogin,
+    onSuccess,
+    onError,
+  });
+
+  const googleLoginMutation = useMutation<string, Error, string>({
+    mutationFn: AuthServices.googleLogin,
+    onSuccess,
+    onError,
+  });
+
+  return { loginMutation, googleLoginMutation };
+};
+
+export default useAuthMutations;
---
+++ src/server/mutation/index.ts
@@ -0,0 +1,1 @@
+export { default as useAuthMutations } from "./auth.mutation";
