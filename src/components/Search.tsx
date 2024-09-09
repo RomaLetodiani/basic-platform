2cf9b184c14d87ea3ec8eed118f3830427b62198
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-09 22:37:30 +0400

    Run Prettier Format

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,28 +1,27 @@
+import FormControl from "@mui/material/FormControl";
+import InputAdornment from "@mui/material/InputAdornment";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 
-import FormControl from '@mui/material/FormControl';
-import InputAdornment from '@mui/material/InputAdornment';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+const Search = () => {
+  return (
+    <FormControl sx={{ width: { xs: "100%", md: "25ch" } }} variant="outlined">
+      <OutlinedInput
+        size="small"
+        id="search"
+        placeholder="Search…"
+        sx={{ flexGrow: 1 }}
+        startAdornment={
+          <InputAdornment position="start" sx={{ color: "text.primary" }}>
+            <SearchRoundedIcon fontSize="small" />
+          </InputAdornment>
+        }
+        inputProps={{
+          "aria-label": "search",
+        }}
+      />
+    </FormControl>
+  );
+};
 
-const  Search = ()=> {
-    return (
-        <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
-            <OutlinedInput
-                size="small"
-                id="search"
-                placeholder="Search…"
-                sx={{ flexGrow: 1 }}
-                startAdornment={
-                    <InputAdornment position="start" sx={{ color: 'text.primary' }}>
-                        <SearchRoundedIcon fontSize="small" />
-                    </InputAdornment>
-                }
-                inputProps={{
-                    'aria-label': 'search',
-                }}
-            />
-        </FormControl>
-    );
-}
-
-export default Search
+export default Search;
