e8cea01b69691a461ea4e877e5408dd5d097d663
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-09 22:35:47 +0400

    Login Page

--- src/pages/auth/login/Login.page.tsx
+++ src/pages/auth/login/Login.page.tsx
@@ -1,36 +1,23 @@
 import Stack from "@mui/material/Stack";
-import { ToggleTheme } from "@/components";
+import { Theme } from "@mui/material/styles";
+import { ThemeSwitcherAbsolute } from "@/components";
 import { LoginCard, LoginContent } from "./components";
+
+const loginPageSX = [
+  { justifyContent: "space-between" },
+  (theme: Theme) => ({
+    backgroundImage: "radial-gradient(ellipse at 70% 51%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+    backgroundSize: "cover",
+    ...theme.applyStyles("dark", {
+      backgroundImage: "radial-gradient(at 70% 51%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
+    }),
+  }),
+];
 
 const LoginPage = () => {
   return (
-    <Stack
-      direction="column"
-      component="main"
-      sx={[
-        { justifyContent: "space-between" },
-        (theme) => ({
-          backgroundImage:
-            "radial-gradient(ellipse at 70% 51%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
-          backgroundSize: "cover",
-          ...theme.applyStyles("dark", {
-            backgroundImage:
-              "radial-gradient(at 70% 51%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
-          }),
-        }),
-      ]}
-    >
-      <Stack
-        direction="row"
-        sx={{
-          justifyContent: "end",
-          position: { sm: "static", md: "fixed" },
-          width: "100%",
-          p: { xs: 2, sm: 4 },
-        }}
-      >
-        <ToggleTheme data-screenshot="toggle-mode" />
-      </Stack>
+    <Stack direction="column" component="main" sx={loginPageSX}>
+      <ThemeSwitcherAbsolute />
       <Stack
         direction={{ xs: "column-reverse", md: "row" }}
         sx={{
