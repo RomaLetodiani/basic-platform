ef96ab575a06c6b1276229e5e6e384e797e7faf6
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-09 22:31:29 +0400

    RHFTextField

--- src/components/RHF/RHFTextField.tsx
+++ src/components/RHF/RHFTextField.tsx
@@ -1,12 +1,17 @@
+import { ReactNode } from "react";
 import { Controller, FieldValues, Path, useFormContext } from "react-hook-form";
 
 import TextField, { type TextFieldProps } from "@mui/material/TextField";
+import FormLabel from "@mui/material/FormLabel";
+import Box from "@mui/material/Box";
 
 type Props<T extends FieldValues> = {
   name: Path<T>;
+  label?: ReactNode;
+  customLabel?: ReactNode;
 } & TextFieldProps;
 
-const RHFTextField = <T extends FieldValues>({ name, ...props }: Props<T>) => {
+const RHFTextField = <T extends FieldValues>({ name, label, customLabel, ...props }: Props<T>) => {
   const { control } = useFormContext();
 
   return (
@@ -14,7 +19,11 @@ const RHFTextField = <T extends FieldValues>({ name, ...props }: Props<T>) => {
       name={name}
       control={control}
       render={({ field, fieldState: { error } }) => (
-        <TextField {...field} {...props} error={!!error} helperText={error?.message} />
+        <Box>
+          {!label && customLabel ? customLabel : null}
+          {label ? <FormLabel htmlFor={name}>{label}</FormLabel> : null}
+          <TextField {...field} {...props} error={!!error} helperText={error?.message} />
+        </Box>
       )}
     />
   );
