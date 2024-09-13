b6414d6422c48bf4d45cb50e3b0a3941cd8794da
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-13 23:11:26 +0400

    Cards

--- src/pages/auth/login/components/index.ts
+++ src/pages/auth/login/components/index.ts
@@ -1,6 +1,3 @@
 export { default as ForgotPassword } from "./ForgotPassword";
-export { default as LoginContent } from "./LoginContent";
-export { default as LoginCard } from "./LoginCard";
-export { default as LoginForm } from "./LoginForm";
-export { default as LoginFormProvider } from "./LoginFormProvider";
-export { type LoginFormData, loginFormInitialValues, loginFormSchema } from "./LoginFormSchema";
+export { default as Content } from "./Content";
+export { default as Cards } from "./Cards";
