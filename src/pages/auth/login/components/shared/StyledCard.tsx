8e94abb7539609be3bc6a4c0d23a27d037545879
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-13 22:23:24 +0400

    Styled Card

---
+++ src/pages/auth/login/components/shared/StyledCard.tsx
@@ -0,0 +1,20 @@
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+
+const StyledCard = styled(Card)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
+  minHeight: "600px",
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  boxShadow:
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles?.("dark", {
+    boxShadow:
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
+  }),
+}));
+
+export default StyledCard;
