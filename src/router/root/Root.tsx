9e1518b15127cd28f31ff9004627bdaf1e9a5ec5
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-13 22:22:47 +0400

    redirectedFrom

--- src/router/root/Root.tsx
+++ src/router/root/Root.tsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom";
-import { Suspense } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { Suspense, useEffect } from "react";
 import RootSuspense from "./Root.suspense";
 
 const Root = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    localStorage.setItem("redirectedFrom", pathname);
+  }, []);
+
   return (
     <Suspense fallback={<RootSuspense />}>
       <Outlet />
