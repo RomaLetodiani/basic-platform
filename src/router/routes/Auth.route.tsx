0a02774d5b557e2c8218269b7b69f921a45ea4ba
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-13 22:23:40 +0400

    Auth Route Refactor

--- src/router/routes/Auth.route.tsx
+++ src/router/routes/Auth.route.tsx
@@ -1,10 +1,14 @@
-import { AuthStore } from "@/stores";
-import { Navigate, Outlet } from "react-router-dom";
+import { AuthStore, OrgStore, ProjectStore } from "@/stores";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const AuthRoute = () => {
   const isLoggedIn = AuthStore((state) => state.isLoggedIn);
+  const lastOrgId = OrgStore((state) => state.lastOrgId);
+  const latestProjectId = ProjectStore((state) => state.latestProjectId);
+  const location = useLocation();
+  const from = location.state.from;
 
-  return isLoggedIn ? <Navigate to="/" /> : <Outlet />;
+  return isLoggedIn && lastOrgId && latestProjectId ? <Navigate to={from} /> : <Outlet />;
 };
 
 export default AuthRoute;
