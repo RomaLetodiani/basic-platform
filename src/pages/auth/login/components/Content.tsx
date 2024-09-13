bed39c6bd088c9b13dde004fd8458ef046432bfe
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-13 23:11:30 +0400

    Content

---
+++ src/pages/auth/login/components/Content.tsx
@@ -0,0 +1,35 @@
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import { loginContentItems } from "../utils";
+
+const Content = () => {
+  return (
+    <Stack
+      sx={{
+        flexDirection: "column",
+        alignSelf: "center",
+        gap: 4,
+        maxWidth: 450,
+        flex: { xs: "unset", sm: 1 },
+      }}
+    >
+      <Box sx={{ display: { xs: "none", md: "flex" } }}>{/*<BotuIcon />*/}</Box>
+      {loginContentItems.map((item, index) => (
+        <Stack key={index} direction="row" sx={{ gap: 2 }}>
+          {item.icon}
+          <div>
+            <Typography gutterBottom sx={{ fontWeight: "medium" }}>
+              {item.title}
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              {item.description}
+            </Typography>
+          </div>
+        </Stack>
+      ))}
+    </Stack>
+  );
+};
+
+export default Content;
