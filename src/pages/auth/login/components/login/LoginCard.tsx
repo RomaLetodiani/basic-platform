57a6c5242063e90834c39f8f0ab51df20f7b67cb
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-13 22:26:00 +0400

    Login Components

---
+++ src/pages/auth/login/components/login/LoginCard.tsx
@@ -0,0 +1,23 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { BotuIcon } from "@/components/custom/icons";
+import { StyledCard } from "../shared";
+import { LoginFormProvider } from ".";
+
+const LoginCard = () => {
+  return (
+    <StyledCard variant="outlined">
+      <Box sx={{ display: { xs: "flex", md: "none" } }}>
+        <BotuIcon />
+      </Box>
+      <Typography
+        component="h1"
+        variant="h4"
+        sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+      ></Typography>
+      <LoginFormProvider />
+    </StyledCard>
+  );
+};
+
+export default LoginCard;
---
+++ src/pages/auth/login/components/login/index.ts
@@ -0,0 +1,3 @@
+export { default as LoginForm } from "./LoginForm";
+export { default as LoginFormProvider } from "./LoginFormProvider";
+export { type LoginFormData, loginFormInitialValues, loginFormSchema } from "./LoginFormSchema";
