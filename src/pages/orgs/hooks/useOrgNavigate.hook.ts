b0143fddb1951e32f5e6872aa5589380b400f401
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-15 02:50:00 +0400

    Org Navigate

---
+++ src/pages/orgs/hooks/useOrgNavigate.hook.ts
@@ -0,0 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { OrgStore } from "@/stores";
+
+const useOrgNavigate = () => {
+  const navigate = useNavigate();
+  const lastOrgId = OrgStore((state) => state.lastOrgId);
+
+  useEffect(() => {
+    if (lastOrgId) {
+      navigate(`/orgs/${lastOrgId}`);
+    } else {
+      navigate("/orgs");
+    }
+  }, [lastOrgId]);
+};
+
+export default useOrgNavigate;
