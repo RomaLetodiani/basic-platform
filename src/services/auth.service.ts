50fd898c540f98dd13825a2b408fd1273ec01071
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-14 19:51:30 +0400

    Refactor Auth Services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,11 +1,13 @@
-import { platformAPI } from "@/lib";
+import { platformAPI, refreshTokenPlatformApi } from "@/lib";
+import { Tokens } from "@/types";
 
 const AuthServices = {
-  localLogin: (body: { email: string; password: string }) =>
+  localLogin: (body: { email: string; password: string }): Promise<Tokens> =>
     platformAPI.post("auth/local/login", body),
-  googleLogin: (access_token: string): Promise<string> =>
+  googleLogin: (access_token: string): Promise<Tokens> =>
     platformAPI.post("auth/google/login", { access_token }),
   getCurrentUser: () => platformAPI.get("auth/me"),
+  refreshTokens: (): Promise<Tokens> => refreshTokenPlatformApi.post("auth/refresh"),
 };
 
 export default AuthServices;
