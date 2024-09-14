381b1f0c91e16a55fa0f265fe63e0fd352cae638
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-14 19:55:24 +0400

    Refactor Auth Store

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,29 +1,36 @@
+import { Tokens } from "@/types";
 import { create } from "zustand";
 
 interface IAuthStore {
   accessToken: string;
+  refreshToken: string;
   isLoggedIn: boolean;
-  setTokens: (accessToken: string) => void;
+  setTokens: ({ accessToken, refreshToken }: Tokens) => void;
   clearTokens: () => void;
 }
 
 const AuthStore = create<IAuthStore>((set) => ({
   accessToken: "",
+  refreshToken: "",
   isLoggedIn: false,
-  setTokens: (accessToken: string) => {
-    set({ accessToken, isLoggedIn: true });
+  setTokens: ({ accessToken, refreshToken }) => {
+    // TODO: Maybe decode access token if there will be data in it
+    set({ accessToken, refreshToken, isLoggedIn: true });
     localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem("refreshToken", refreshToken);
   },
   clearTokens: () => {
     set({ accessToken: "", isLoggedIn: false });
     localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
   },
 }));
 
 export default AuthStore;
 
 const accessToken = localStorage.getItem("accessToken");
+const refreshToken = localStorage.getItem("refreshToken");
 
-if (accessToken) {
-  AuthStore.getState().setTokens(accessToken);
+if (accessToken && refreshToken) {
+  AuthStore.getState().setTokens({ accessToken, refreshToken });
 }
