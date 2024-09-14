a148a3e8f2ce6a71b95c10c3efe1a51fc45f5f30
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-14 20:06:26 +0400

    body -> formData

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,8 +2,13 @@ import { platformAPI, refreshTokenPlatformApi } from "@/lib";
 import { Tokens } from "@/types";
 
 const AuthServices = {
-  localLogin: (body: { email: string; password: string }): Promise<Tokens> =>
-    platformAPI.post("auth/local/login", body),
+  localLogin: (body: { email: string; password: string }): Promise<Tokens> => {
+    const formData = new FormData();
+    formData.append("username", body.email);
+    formData.append("password", body.password);
+
+    return platformAPI.post("auth/local/login", formData);
+  },
   googleLogin: (access_token: string): Promise<Tokens> =>
     platformAPI.post("auth/google/login", { access_token }),
   getCurrentUser: () => platformAPI.get("auth/me"),
