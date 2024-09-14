c15b6c54414edb52bfb14c3e7109f9566ffa5e65
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-15 03:43:48 +0400

    Refactor Orgs Query

--- src/server/query/orgs.query.ts
+++ src/server/query/orgs.query.ts
@@ -1,17 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { baseQuery } from ".";
 import { OrgsServices } from "@/services";
+import { AuthStore } from "@/stores";
+import { Org } from "@/types";
 
 export const useOrgsQuery = () => {
+  const isLoggedIn = AuthStore((state) => state.isLoggedIn);
+
   const getOrgs = async () =>
     await OrgsServices.getCurrentUserOrgs().catch((error) => {
       console.error(error);
       throw new Error(error);
     });
 
-  return useQuery({
+  return useQuery<Org[]>({
     queryKey: ["orgs"],
     queryFn: getOrgs,
+    enabled: isLoggedIn,
     ...baseQuery,
+    placeholderData: [],
   });
 };
