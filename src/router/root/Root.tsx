131dae4b8bfb7cd844af57385754880243f551eb
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-15 04:56:07 +0400

    loading on root while setting last org id

--- src/router/root/Root.tsx
+++ src/router/root/Root.tsx
@@ -1,8 +1,20 @@
 import { Outlet } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
+import { OrgStore } from "@/stores";
 import RootSuspense from "./Root.suspense";
 
 const Root = () => {
+  const setLastOrgId = OrgStore((state) => state.setLastOrgId);
+  const orgLoading = OrgStore((state) => state.loading);
+
+  useEffect(() => {
+    const lastOrgId = localStorage.getItem("lastOrgId");
+
+    if (lastOrgId) setLastOrgId(lastOrgId);
+  }, [setLastOrgId]);
+
+  if (orgLoading) return <RootSuspense />;
+
   return (
     <Suspense fallback={<RootSuspense />}>
       <Outlet />
