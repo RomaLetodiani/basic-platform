14f32ec796a67ab545962559ea302ed11176a685
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-15 06:53:28 +0400

    Refactor Org and Project Stores

--- src/stores/org.store.ts
+++ src/stores/org.store.ts
@@ -2,22 +2,28 @@ import { create } from "zustand";
 
 interface IOrgStore {
   lastOrgId: string;
-  loading: boolean;
   setLastOrgId: (latestOrgId: string) => Promise<void>;
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
 }
 
 const OrgStore = create<IOrgStore>((set) => ({
   lastOrgId: "",
-  loading: false,
+  loading: true,
+  setLoading: (loading: boolean) => set({ loading }),
   setLastOrgId: async (lastOrgId: string) => {
     set({ loading: true });
     // TODO: Check Permissions
-    const fakeApiCall = () => new Promise((resolve) => setTimeout(resolve, 5000));
-    await fakeApiCall().then(() => {
-      console.log("Permissions checked successfully");
-    });
-    set({ lastOrgId, loading: false });
-    localStorage.setItem("lastOrgId", lastOrgId);
+    const fakeApiCall = () => new Promise((resolve) => setTimeout(resolve, 4000));
+    await fakeApiCall()
+      .then(() => {
+        console.log("Permissions checked successfully for org");
+        // throw new Error("Permissions checked successfully for org");
+      })
+      .finally(() => {
+        set({ lastOrgId, loading: false });
+        localStorage.setItem("lastOrgId", lastOrgId);
+      });
   },
 }));
 
--- src/stores/project.store.ts
+++ src/stores/project.store.ts
@@ -2,22 +2,27 @@ import { create } from "zustand";
 
 interface IProjectStore {
   lastProjectId: string;
-  setLastProjectId: (lastProjectId: string) => void;
+  loading: boolean;
+  setLastProjectId: (lastProjectId: string) => Promise<void>;
 }
 
 const ProjectStore = create<IProjectStore>((set) => ({
   lastProjectId: "",
-  setLastProjectId: (lastProjectId: string) => {
+  loading: false,
+  setLastProjectId: async (lastProjectId: string) => {
+    set({ loading: true });
     // TODO: Check Permissions
-    set({ lastProjectId });
-    localStorage.setItem("lastProjectId", lastProjectId);
+    const fakeApiCall = () => new Promise((resolve) => setTimeout(resolve, 2000));
+    await fakeApiCall()
+      .then(() => {
+        console.log("Permissions checked successfully for project");
+        // throw new Error("Permissions checked successfully for project");
+      })
+      .finally(() => {
+        set({ lastProjectId, loading: false });
+        localStorage.setItem("lastProjectId", lastProjectId);
+      });
   },
 }));
 
 export default ProjectStore;
-
-const lastProjectId = localStorage.getItem("lastProjectId");
-
-if (lastProjectId) {
-  ProjectStore.getState().setLastProjectId(lastProjectId);
-}
