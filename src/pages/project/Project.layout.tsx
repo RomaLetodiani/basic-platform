6981d917bedf9ddeb014e8bf497208d94ae9ea26
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-15 08:13:11 +0400

    ProjectLayout

--- src/pages/project/Project.layout.tsx
+++ src/pages/project/Project.layout.tsx
@@ -1,9 +1,17 @@
+import { Outlet } from "react-router-dom";
 import { useProjectNavigate } from "./hooks";
 
 const ProjectLayout = () => {
   useProjectNavigate();
 
-  return <div>ProjectLayout</div>;
+  return (
+    <div>
+      <div>Project layout</div>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectLayout;
