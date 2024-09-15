55b03f636c833b73c9d9b0d0648ef2388fc6fe44
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-15 06:51:56 +0400

    Project Chat Pages

--- src/pages/chats/index.ts
+++
@@ -1,5 +0,0 @@
-import { lazy } from "react";
-
-const ChatsPage = lazy(() => import("./chats.page"));
-
-export { ChatsPage };
--- src/pages/project/index.ts
+++ src/pages/project/index.ts
@@ -1,5 +1,21 @@
 import { lazy } from "react";
 
 const ProjectLayout = lazy(() => import("./Project.layout"));
+const ChatPage = lazy(() => import("./pages/chat/Chat.page"));
+const ChatViewPage = lazy(() => import("./pages/chat/view/View.page"));
+const DialogFlowPage = lazy(() => import("./pages/dialogFlow/DialogFlow.page"));
+const Extensions = lazy(() => import("./pages/extensions/Extensions.page"));
+const Integrations = lazy(() => import("./pages/integrations/Integrations.page"));
+const KnowledgeBase = lazy(() => import("./pages/knowledgeBase/KnowledgeBase.page"));
+const ProjectOverviewPage = lazy(() => import("./pages/overview/Overview.page"));
 
-export { ProjectLayout };
+export {
+  ProjectLayout,
+  ChatPage,
+  ChatViewPage,
+  DialogFlowPage,
+  Extensions,
+  Integrations,
+  KnowledgeBase,
+  ProjectOverviewPage,
+};
