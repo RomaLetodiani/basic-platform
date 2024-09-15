12c4f54487f009665483ded8c34b2478cbc06c3b
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-15 04:55:23 +0400

    org store update

--- src/stores/org.store.ts
+++ src/stores/org.store.ts
@@ -2,24 +2,23 @@ import { create } from "zustand";
 
 interface IOrgStore {
   lastOrgId: string;
+  loading: boolean;
   setLastOrgId: (latestOrgId: string) => Promise<void>;
 }
 
 const OrgStore = create<IOrgStore>((set) => ({
   lastOrgId: "",
+  loading: false,
   setLastOrgId: async (lastOrgId: string) => {
+    set({ loading: true });
     // TODO: Check Permissions
-    const fakeApiCall = () => new Promise((resolve) => setTimeout(resolve, 1000));
-    await fakeApiCall();
-    set({ lastOrgId });
+    const fakeApiCall = () => new Promise((resolve) => setTimeout(resolve, 5000));
+    await fakeApiCall().then(() => {
+      console.log("Permissions checked successfully");
+    });
+    set({ lastOrgId, loading: false });
     localStorage.setItem("lastOrgId", lastOrgId);
   },
 }));
 
 export default OrgStore;
-
-const lastOrgId = localStorage.getItem("lastOrgId");
-
-if (lastOrgId) {
-  OrgStore.getState().setLastOrgId(lastOrgId);
-}
