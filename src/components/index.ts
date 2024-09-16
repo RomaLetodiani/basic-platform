8fd37b1e06da4fe3724d1a3275346afb6265c3b8
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-16 20:40:23 +0400

    Add MenuButton component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Search } from "./Search";
 export { default as ToggleTheme } from "./ToggleTheme";
 export { default as ThemeSwitcherAbsolute } from "./ToggleTheme.absolute";
 export { default as Loading } from "./Loading";
+export { default as MenuButton } from "./MenuButton";
