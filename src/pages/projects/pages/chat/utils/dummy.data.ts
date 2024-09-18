1c899ca38bbaf1fad9a27cc97aca264348ce6b81
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-18 21:49:03 +0400

    change ids from number to strings

--- src/pages/projects/pages/chat/utils/dummy.data.ts
+++ src/pages/projects/pages/chat/utils/dummy.data.ts
@@ -3,25 +3,25 @@ export const chatData = [
     section: "Active",
     chats: [
       {
-        id: 1,
+        id: "1",
         avatar: "/static/images/avatar/1.jpg",
         primary: "Brunch this weekend?",
         secondary: "Ali Connors — I'll be in your neighborhood doing errands this…",
       },
       {
-        id: 2,
+        id: "2",
         avatar: "/static/images/avatar/1.jpg",
         primary: "Brunch this weekend?",
         secondary: "Ali Connors — I'll be in your neighborhood doing errands this…",
       },
       {
-        id: 3,
+        id: "3",
         avatar: "/static/images/avatar/1.jpg",
         primary: "Brunch this weekend?",
         secondary: "Ali Connors — I'll be in your neighborhood doing errands this…",
       },
       {
-        id: 4,
+        id: "4",
         avatar: "/static/images/avatar/1.jpg",
         primary: "Brunch this weekend?",
         secondary: "Ali Connors — I'll be in your neighborhood doing errands this…",
@@ -32,19 +32,19 @@ export const chatData = [
     section: "Today",
     chats: [
       {
-        id: 5,
+        id: "5",
         avatar: "/static/images/avatar/3.jpg",
         primary: "Oui Oui",
         secondary: "Sandra Adams — Do you have Paris recommendations? Have you ever…",
       },
       {
-        id: 6,
+        id: "6",
         avatar: "/static/images/avatar/3.jpg",
         primary: "Oui Oui",
         secondary: "Sandra Adams — Do you have Paris recommendations? Have you ever…",
       },
       {
-        id: 7,
+        id: "7",
         avatar: "/static/images/avatar/1.jpg",
         primary: "Brunch this weekend?",
         secondary: "Ali Connors — I'll be in your neighborhood doing errands this…",
@@ -55,25 +55,25 @@ export const chatData = [
     section: "Aug 3",
     chats: [
       {
-        id: 8,
+        id: "8",
         avatar: "/static/images/avatar/1.jpg",
         primary: "Brunch this weekend?",
         secondary: "Ali Connors — I'll be in your neighborhood doing errands this…",
       },
       {
-        id: 9,
+        id: "9",
         avatar: "/static/images/avatar/1.jpg",
         primary: "Brunch this weekend?",
         secondary: "Ali Connors — I'll be in your neighborhood doing errands this…",
       },
       {
-        id: 10,
+        id: "10",
         avatar: "/static/images/avatar/1.jpg",
         primary: "Brunch this weekend?",
         secondary: "Ali Connors — I'll be in your neighborhood doing errands this…",
       },
       {
-        id: 11,
+        id: "11",
         avatar: "/static/images/avatar/1.jpg",
         primary: "Brunch this weekend?",
         secondary: "Ali Connors — I'll be in your neighborhood doing errands this…",
