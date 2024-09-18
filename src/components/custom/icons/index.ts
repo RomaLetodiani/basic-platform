bfca86e56c038fe272e6b9e422b86e16fe650b4a
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-18 21:08:45 +0400

    DashboardIcon

--- src/components/custom/icons/index.ts
+++ src/components/custom/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as BotuIcon } from "./Botu.icon";
 export { default as GoogleIcon } from "./Google.icon";
 export { default as MicrosoftIcon } from "./Microsoft.icon";
+export { default as DashboardIcon } from "./Dashboard.icon";
