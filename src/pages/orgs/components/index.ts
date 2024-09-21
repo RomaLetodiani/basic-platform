fe0186e10ce5a9602df1447e1be673b1747dc64f
RomaLetodiani <roman.letodiani03@gmail.com>
2024-09-21 07:06:33 +0400

    orgs components

---
+++ src/pages/orgs/components/index.ts
@@ -0,0 +1,5 @@
+export { default as CardAlert } from "./CardAlert";
+export { default as MenuContent } from "./MenuContent";
+export { default as SideMenuMobile } from "./SideMenuMobile";
+export { default as SideMenu } from "./SideMenu";
+export { default as Navbar } from "./Navbar";
